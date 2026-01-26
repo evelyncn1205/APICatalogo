654042c373ba0930ca3fb134c3f4e9ce31f910d8
Evelyncn1205 <evelynrx_rj@hotmail.com>
2026-01-26 17:11:52 +0000

    Criação da Pasta Model, classe Categoria, Classe Produtos, connection string

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext   
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base (option)
+        {
+                
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+    }
+}
--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,8 +1,19 @@
-﻿namespace APICatalogo.Models
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+
+namespace APICatalogo.Models
 {
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
         public int CategoriaID { get; set; }
+        public string? Nome { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; }
 
     }
 }
